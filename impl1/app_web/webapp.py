b405833f41a254c39f11126259e439da0e9357fc
Chris Joakim <christopher.joakim@gmail.com>
2024-05-31 15:58:47 -0400

    Added fastapi_msal to webapp.py (not enabled by default), enhanced ConfigService for msal

--- impl1/app_web/webapp.py
+++ impl1/app_web/webapp.py
@@ -13,7 +13,7 @@ import httpx
 
 from dotenv import load_dotenv
 
-from fastapi import FastAPI, Request, Response, Form, status, Depends
+from fastapi import FastAPI, Request, Response, Form, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
